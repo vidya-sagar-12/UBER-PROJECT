9b586ed2f14df51b37cb33d27c55d0f6110dc02d
vidya-sagar-12 <vsj.ka32@gmail.com>
2025-04-25 12:42:34 +0530

    user authentication done

--- Backend/controllers/user-controller.js
+++ Backend/controllers/user-controller.js
@@ -1,6 +1,7 @@
 const userModel = require('../models/user_model.js');
 const userService = require('../services/user-services.js');
 const { validationResult } = require('express-validator');
+const blacklistTokenModel = require('../models/blacklsitToken-model.js');
 
 
 module.exports.registerUser = async (req, res, next) => {
@@ -23,3 +24,47 @@ module.exports.registerUser = async (req, res, next) => {
     const token = user.generateAuthToken();
     res.status(201).json({ token, user });
 }
+
+module.exports.loginUser = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email }).select('+password');
+
+    if (!user) {
+        return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+        return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    const token = user.generateAuthToken();
+
+    res.cookie('token', token);
+
+    res.status(200).json({ token, user });
+}
+
+module.exports.getUserProfile = async (req, res, next) => {
+    
+    res.status(200).json(req.user);
+    
+}
+
+module.exports.logoutUser = async (req, res, next) => {
+    res.clearCookie('token');
+    const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+
+    await blacklistTokenModel.create({ token });
+
+    res.status(200).json({ message: 'Logged out successfully' });
+}
+
+
+
--- Backend/models/user_model.js
+++ Backend/models/user_model.js
@@ -31,7 +31,7 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.methods.generateAuthToken = function () {
-    const token = jwt.sign({ id: this._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+    const token = jwt.sign({ id: this._id }, process.env.JWT_SECRET, { expiresIn: '24h' });
     return token;
 }
 
